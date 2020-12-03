df979c0ec925ea4ce097627471c868f837babfe1
David Siemionko <dsiemionko4406@stu.d214.org>
2020-12-03 16:05:59 -0500

    Initial Commit

---
+++ ConditionalsLecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConditionalsLecture
+//
+//  Created by user180860 on 12/3/20.
+//  Copyright © 2020 user180860. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
